82eab5a8e29085521eae8643fb0ce4f5f1b4c7b8
KhromeM <khrome@nyu.edu>
2024-05-08 16:27:25 -0400

    m

---
+++ src/firebaseAuth/data.js
@@ -0,0 +1,60 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import {
+	getFirestore,
+	doc,
+	onSnapshot,
+	getDoc,
+	setDoc,
+	updateDoc,
+} from "firebase/firestore";
+import app from "./firebase.js";
+import { useAuth } from "./auth.js";
+
+const db = getFirestore(app);
+
+const userContext = createContext();
+export const useUser = () => useContext(userContext);
+
+export const UserProvider = ({ children }) => {
+	const data = useGetUser();
+	return <userContext.Provider value={data}>{children}</userContext.Provider>;
+};
+
+const fetchDoc = (collection, document, setter) => {
+	getDoc(doc(db, collection, document)).then((doc) => {
+		setter(doc.data());
+	});
+};
+const createDoc = (collection, document, data) => {
+	setDoc(doc(db, collection, document), data)
+		.then(() => {
+			console.log("Document Created");
+		})
+		.catch((error) => {
+			console.log("Document creation error: ", error);
+		});
+};
+const updateDoc = (collection, document, data) => {
+	updateDoc(doc(db, collection, document), data)
+		.then(() => {
+			console.log("Document Created");
+		})
+		.catch((error) => {
+			console.log("Document creation error: ", error);
+		});
+};
+
+const useGetUser = () => {
+	const { user } = useAuth();
+	const [userData, setUserData] = useState(false);
+
+	useEffect(() => {
+		if (user) {
+			fetchDoc("users", user.uid, setUserData);
+		} else {
+			setUserData(false);
+		}
+	}, [user]);
+
+	return { userData, createDoc, updateDo, fetchDoc };
+};
